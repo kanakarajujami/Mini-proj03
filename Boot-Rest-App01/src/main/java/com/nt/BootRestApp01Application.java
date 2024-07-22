f45d90de41d947308f45d919e3c9817b58f90bbe
kanakaraju <kanakarajujami409@gmail.com>
2024-07-22 01:50:41 -0900

    project pushed

---
+++ Boot-Rest-App01/src/main/java/com/nt/BootRestApp01Application.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRestApp01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootRestApp01Application.class, args);
+	}
+
+}
---
+++ Boot-Rest-App01/src/main/java/com/nt/ms/ActorOperationsController.java
@@ -0,0 +1,17 @@
+package com.nt.ms;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/actor-api")
+public class ActorOperationsController {
+	@GetMapping("/wish")
+	public ResponseEntity<String> wishMessage(){
+		   return new ResponseEntity<String>("GoodMorning::kanakaraju",HttpStatus.OK);
+	}
+
+}
