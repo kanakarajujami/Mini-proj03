7447c813856f00648a2bf626ce7471fabf943b30
hp <hp@DESKTOP-FHOCCBM>
2024-09-24 16:57:21 +0800

    jenkins webapp added

---
+++ JenkinsProj01-WebApp/src/main/java/com/nt/servelt/WishMessageOperations.java
@@ -0,0 +1,33 @@
+package com.nt.servelt;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/weburl")
+public class WishMessageOperations extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	 	     //set response content type
+		    resp.setContentType("text/html");
+		    //get pdf writer
+		    PrintWriter pw=resp.getWriter();
+		    //set response
+		    pw.write("<h1 style='color:red;text-align:center'>Good Morning</h1><br><br>");
+		    //set home page
+		    pw.write("<a href='index.jsp'>GoToHome</a>");
+		    //close print writer
+		    pw.close();
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+      doGet(req, resp);
+	}
+
+}
---
+++ JenkinsProj01-WebApp/src/main/webapp/index.jsp
@@ -0,0 +1,4 @@
+
+
+<h2 style="color:red;text-align:center">Jenkins Web Application</h2><br><br>
+<a href="weburl">Show Date Time</a>
