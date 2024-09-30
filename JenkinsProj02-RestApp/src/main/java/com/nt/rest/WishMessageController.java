2bd4e0d063e133ae9aa15153601505b3d76cfa2a
hp <hp@DESKTOP-FHOCCBM>
2024-09-30 13:40:53 +0800

    jenkins02 projejct commited

---
+++ JenkinsProj02-RestApp/src/main/java/com/nt/rest/WishMessageController.java
@@ -0,0 +1,16 @@
+package com.nt.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/wish")
+public class WishMessageController {
+	@GetMapping("/display")
+   public ResponseEntity<String> showWishMessage(){
+	      return new ResponseEntity<String>("Good Morning To Evernone",HttpStatus.OK);
+   }
+}
