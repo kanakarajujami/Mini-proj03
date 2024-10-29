3c0564a341f71169100746e39ecb4f0fbff6c177
hp <hp@DESKTOP-FHOCCBM>
2024-10-29 14:08:05 +0800

    wish message project added

---
+++ WishMessage/src/main/java/com/nt/WishMessageController.java
@@ -0,0 +1,33 @@
+package com.nt;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/wishurl")
+public class WishMessageController extends HttpServlet {
+
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	              //create print writer object
+		PrintWriter writer=response.getWriter();
+		//set content type
+		response.setContentType("html/text");
+		writer.print("<h2 style='color:red,text-align:center,margin-top:350px;'>Hello,Good Morning to EveryOne</h2>");
+		writer.print("<a a='wishurl'>GotoHome</a>");
+		//close writer stream object;
+		writer.close();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		doGet(request, response);
+	}
+
+}
---
+++ WishMessage/src/main/webapp/index.jsp
@@ -0,0 +1,3 @@
+<h1 style="color:blue,text-align:center;">Hello Docker Web application</h1>
+<br><br>
+<a href="wishurl">ShowWishMessage</a>
